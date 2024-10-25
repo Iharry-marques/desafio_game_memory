398a63f99a9d161051e160c02f4e22caffd8c704
Iharry-marques <fabio.marques.melo@gmail.com>
2024-10-25 14:25:45 -0300

    Creating winning conditions

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,48 +1,58 @@
 const emojis = [
-    "🐱",
-    "🐱",
-    "🐵",
-    "🐵",
-    "🐺",
-    "🐺",
-    "🐶",
-    "🐶",
-    "🦁",
-    "🦁",
-    "🐸",
-    "🐸",
-    "🐲",
-    "🐲",
-    "🐋",
-    "🐋",
+  "🐱",
+  "🐱",
+  "🐵",
+  "🐵",
+  "🐺",
+  "🐺",
+  "🐶",
+  "🐶",
+  "🦁",
+  "🦁",
+  "🐸",
+  "🐸",
+  "🐲",
+  "🐲",
+  "🐋",
+  "🐋",
 ];
 let openCards = [];
 
-let shuffleEmojis = emojis.sort(()=>(Math.random() > 0.5 ? 2: -1));
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
 
-for(let i = 0; i<emojis.length; i++){
-    let box = document.createElement("div");
-    box.className = "item";
-    box.innerHTML = shuffleEmojis[i];
-    box.onclick = handleClick;
-    document.querySelector(".game").appendChild(box)
+for (let i = 0; i < emojis.length; i++) {
+  let box = document.createElement("div");
+  box.className = "item";
+  box.innerHTML = shuffleEmojis[i];
+  box.onclick = handleClick;
+  document.querySelector(".game").appendChild(box);
 }
 
-function handleClick(){
-    if(openCards.length < 2){
-        this.classList.add("boxOpen");
-        openCards.push(this)
-    }
+function handleClick() {
+  if (openCards.length < 2) {
+    this.classList.add("boxOpen");
+    openCards.push(this);
+  }
 
-    if(openCards.length == 2){
-        setTimeout(checkMatch, 500)
-    }
+  if (openCards.length == 2) {
+    setTimeout(checkMatch, 500);
+  }
+
+  console.log(openCards);
 }
 
-function checkMatch () {}
+function checkMatch() {
+  if (openCards[0].innerHTML === openCards[1].innerHTML) {
+    openCards[0].classList.add("boxMatch");
+    openCards[1].classList.add("boxMatch");
+  } else {
+    openCards[0].classList.remove("boxOpen");
+    openCards[1].classList.remove("boxOpen");
+  }
 
+  openCards = [];
 
-
-
-
-
+  if(document.querySelectorAll(".boxMatch").length === emojis.length){
+    alert("Você venceu !")
+  }
+}
